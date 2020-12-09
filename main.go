3dd3eab21e0a103391f39b3d289514f329833daa
zhaogtianzeng_i <zhaotianzeng_i@didiglobal.com>
2020-12-09 11:36:25 +0800

    main

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"study/test"
 )
 
 func main() {
-	fmt.Println("ss")
-	fmt.Println("test222" + "s")
-	s := test.Student{Name: "ss"}
-	fmt.Println(s)
+	fmt.Println("main")
 }
